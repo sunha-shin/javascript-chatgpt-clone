b3d8d637695c650545af7e7f859c9166345b2bbc
Sunha Shin <shinsunha11@gmail.com>
2024-02-15 02:57:53 -0500

    write readme

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const getMessage = async () => {
     showLoadingIcon(); // 로딩 아이콘을 표시
 
     try {
-        const response = await fetch('https://open-api.jejucodingcamp.workers.dev/', {
+        const response = await fetch('https://open-api.jejucodingcamp.workers.de/', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify([{ "role": "user", "content": inputElement.value }])
@@ -55,6 +55,7 @@ const historyElementCreator = (inputValue) => {
     const historyP = document.createElement('p');
     historyP.textContent = inputValue;
     
+    // 클릭 시에만 실행되어 화면에 결과 값 보여줌
     historyP.addEventListener('click', () => {
         outputElement.innerHTML = '';
         changeInput(historyP.textContent);
@@ -66,7 +67,8 @@ const historyElementCreator = (inputValue) => {
         }
     });
 
-    historyElement.append(historyP);
+    historyElement.append(historyP);    
+    // getMessage가 불려야 실행됨 --> 엔터나 버튼클릭 이벤트 필요
 };
 
 // 입력 필드 값을 변경하는 함수
